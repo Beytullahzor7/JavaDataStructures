7360c7d52ede825d05bc651d4c6371c902d99c6d
Beytullahzor7 <beytullahzor7@gmail.com>
2022-03-05 23:51:49 +0300

    HashCode

--- src/main/java/com/example/WorkingWithMaps.java
+++ src/main/java/com/example/WorkingWithMaps.java
@@ -2,9 +2,23 @@ package com.example;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class WorkingWithMaps {
     public static void main(String[] args) {
+        Map<Person, Diamond> diamondMap = new HashMap<>();
+
+        //Person -> Key | Diamond -> Value
+        diamondMap.put(new Person("Johny"), new Diamond("African Diamond"));
+
+        System.out.println(new Person("Johny").hashCode());
+        System.out.println(new Person("Johny").hashCode());
+        System.out.println(diamondMap.get(new Person("Johny")));
+
+    }
+
+
+    private static void maps(){
         Map<Integer, Person> personMap = new HashMap<>();
         personMap.put(1, new Person("Alex"));
         personMap.put(2, new Person("Maria"));
@@ -24,5 +38,33 @@ public class WorkingWithMaps {
         });
     }
 
-    record Person(String name){}
+    static class Person{
+        String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+
+    record Diamond(String name){}
 }
