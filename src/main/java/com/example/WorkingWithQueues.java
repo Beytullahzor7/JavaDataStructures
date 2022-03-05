f426b5e9f0e9f54749a483a727c69ff9387027f7
Beytullahzor7 <beytullahzor7@gmail.com>
2022-03-05 18:56:12 +0300

    LinkedList Added

--- src/main/java/com/example/WorkingWithQueues.java
+++ src/main/java/com/example/WorkingWithQueues.java
@@ -1,10 +1,31 @@
 package com.example;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Queue;
 
 public class WorkingWithQueues {
     public static void main(String[] args) {
+
+        LinkedList<Person> linkedList = new LinkedList<>();
+        linkedList.add(new Person("Alex", 25));
+        linkedList.add(new Person("Mary", 23));
+        linkedList.addFirst(new Person("Ali", 24));
+
+         ListIterator<Person> listIterator = linkedList.listIterator();
+         while (listIterator.hasNext()){
+             System.out.println(listIterator.next());
+         }
+        System.out.println("*****");
+
+         while (listIterator.hasPrevious()){
+             System.out.println(listIterator.previous());
+         }
+
+
+    }
+
+    private static void queues(){
         Queue<Person> supermarket = new LinkedList<>();
 
         supermarket.add(new Person("Alex", 19));
@@ -17,7 +38,6 @@ public class WorkingWithQueues {
         System.out.println(supermarket.poll()); //It gives us the first element and delete it
         System.out.println(supermarket.size());
         System.out.println(supermarket.peek());
-
     }
 
     record Person(String name, int age){}
